5d386ea4004a7d296d3f9fe33b505e76f728a508
hankliu <397694072@qq.com>
2024-02-15 00:03:53 +0800

    feat(scripts): add fetch issues and labels libs

--- .bin/constant.js
+++ .bin/constant.js
@@ -3,3 +3,23 @@ const LabelZhName = {
   algorithm: '算法',
   project: ''
 };
+
+const GitHubApiVersion = "2022-11-28";
+
+const GithubOwner = "hankliu62";
+
+// 博客Repo
+const GithubRepoBlog = "hankliu62.github.com";
+
+// 面试题Repo
+const GithubRepoInterview = 'interview';
+
+const AccessToken = ['g', 'h', 'p', '_', 'E', 'v', 'x', 'J', 'M', 'b', 'F', 'v', 'K', 'g', 'B', 'w', 'r', 'g', 'L', 'N', '2', '5', 'H', 'Y', 'o', '1', 'v', 'n', 'g', 'R', '9', '4', '9', '4', '2', 'd', 'P', 'p', 'F', 'p'];
+
+module.exports = {
+  AccessToken,
+  GitHubApiVersion,
+  GithubOwner,
+  GithubRepoBlog,
+  GithubRepoInterview
+}
